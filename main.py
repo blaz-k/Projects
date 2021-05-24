6293587385edd8dfaa9fc1d3c988cc71ef02dcc8
blaz <blazyy@gmail.com>
2021-05-24 11:59:36 +0200

    no message

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqla_wrapper import SQLAlchemy
 
 
-#db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
-#db = SQLAlchemy(db_url)
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(db_url)
+
+
+#class User
 
 
 app = Flask(__name__)
 
+#db.create_all()
 
 # in alphabetical order
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 @app.route("/")
@@ -29,7 +38,13 @@ def login():
 
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
-    return render_template("registration.html")
+    if request.method == "GET":
+        return render_template("registration.html")
+   # elif request.method == "POST":
+#dobi vse podatke iz baze
+#ce user ne obstaja naredimo novega. check in base
+#check if password == repeat
+
 
 
 if __name__ == "__main__":
