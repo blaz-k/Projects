3ab5e182f3f11151286e7721436c3e7a0f29663b
blaz <blazyy@gmail.com>
2021-05-28 08:35:58 +0200

    no message

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ class CarAd(db.Model):
 
 class CarAdInterest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    #ad_id je cardda.id
+    ad_id = CarAd.id
+    #seller id = user.id
+    seller_id = User.id
     interest_name = db.Column(db.String, unique=False)
     interest_surname = db.Column(db.String, unique=False)
     interest_email = db.Column(db.String, unique=False)
@@ -76,6 +80,7 @@ def contact():
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
+    # MORAMO POSLATI VSE INTERESTE KI SE NANASAJO NA UPORABNIKA
     session_cookie = request.cookies.get("session")
 
     if session_cookie:
@@ -85,6 +90,47 @@ def dashboard():
 
     return render_template("error.html")
 
+"""
+@app.route("/dashboard/edit-profile", methods=["GET", "POST"])
+def dashboard_edit_profile():
+    session_cookie = request.cookies.get("session")
+
+    if session_cookie:
+        user = db.query(User).filter_by(session_token=session_cookie).first()
+
+        if not user:
+            return "You are not logged-in!!!"
+
+    else:
+        return "You are not logged in"
+
+    if request.method == "GET":
+        return render_template("dashboard-edit-profile.html", user=user)
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        country = request.form.get("country")
+        postal_code = request.form.get("postal-code")
+        email = request.form.get("user-email")
+        telephone = request.form.get("telephone")
+        password = request.form.get("password")
+        repeat = request.form.get("repeat")
+
+        user.username = username
+        user.first_name = first_name
+        user.last_name = last_name
+        user.country = country
+        user.postal_code = postal_code
+        user.email = email
+        user.telephone = telephone
+        user.password = password
+        user.repeat = repeat
+        user.save()
+
+        return redirect(url_for("dashboard"))
+"""
 
 @app.route("/")
 def home():
@@ -102,10 +148,6 @@ def interest():
         interest_surname = request.form.get("interest-surname")
         interest_email = request.form.get("interest-email")
         interest_telephone = request.form.get("interest-telephone")
-        print("interest name {}".format(interest_name))
-        print("interest surname {}".format(interest_surname))
-        print("interest email {}".format(interest_email))
-        print("interest telephone {}".format(interest_telephone))
 
         new_interest = CarAdInterest(interest_name=interest_name, interest_surname=interest_surname,
                                      interest_email=interest_email, interest_telephone=interest_telephone)
